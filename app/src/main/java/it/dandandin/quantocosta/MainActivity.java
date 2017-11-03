5143d8b363d42b984b3c647a2c82757b48d22962
Magnetic_dud <mail@dandandin.it>
2017-11-03 20:07:34 +0100

    scrive una quantità immaginaria

--- app/src/main/java/it/dandandin/quantocosta/MainActivity.java
+++ app/src/main/java/it/dandandin/quantocosta/MainActivity.java
@@ -2,12 +2,32 @@ package it.dandandin.quantocosta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * Ma quanto costa?
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Scrive 1 giusto per vedere se funziona
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * Mostra la quantità
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantitytextView);
+        quantityTextView.setText("" + number);
     }
 }
