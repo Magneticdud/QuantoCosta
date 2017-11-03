ef8bb9af493e593aa3e06363ed4fb7facf0bb687
Magnetic_dud <mail@dandandin.it>
2017-11-03 20:15:30 +0100

    scrive un prezzo immaginario

--- app/src/main/java/it/dandandin/quantocosta/MainActivity.java
+++ app/src/main/java/it/dandandin/quantocosta/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * Ma quanto costa?
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         display(1);
+        displayPrice(2);
     }
 
     /**
@@ -29,5 +31,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantitytextView);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * Mostra il prezzo.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.pricetextView);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
