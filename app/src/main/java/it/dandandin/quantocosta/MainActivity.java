e1c20518bcb8be5c9d26b090ac2c2aa36850f7c4
Magnetic_dud <mail@dandandin.it>
2017-11-04 19:47:28 +0100

    adesso almeno è utile

--- app/src/main/java/it/dandandin/quantocosta/MainActivity.java
+++ app/src/main/java/it/dandandin/quantocosta/MainActivity.java
@@ -3,6 +3,7 @@ package it.dandandin.quantocosta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -17,12 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int quantita = 0;
+    double unitPrice = 2.2;
+
+    public void increase(View view) {
+        quantita++;
+        display(quantita);
+    }
+
+    public void decrease(View view) {
+        quantita--;
+        if (quantita<0)
+        {
+            quantita = 0;
+        }
+        display(quantita);
+    }
+
     /**
-     * Scrive 1 giusto per vedere se funziona
+     * Calcola
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2);
+        display(quantita);
+        displayPrice(quantita * unitPrice);
+        LinearLayout linearPrezzo = (LinearLayout) findViewById(R.id.linearprezzo);
+        linearPrezzo.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -36,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Mostra il prezzo.
      */
-    private void displayPrice(int number) {
+    private void displayPrice(double number) {
         TextView priceTextView = (TextView) findViewById(R.id.pricetextView);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
